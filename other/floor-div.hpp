c32bf06f59543b241a88e0637de1d92271f7fe4b
ei1333 <ei1333hobby@gmail.com>
2026-06-01 10:26:30 +0000

    add validation

--- other/floor-div.hpp
+++ other/floor-div.hpp
@@ -1,4 +1,8 @@
+#include <type_traits>
+
 template <typename T>
 T floor_div(T n, T d) {
+  static_assert(is_integral<T>::value and is_signed<T>::value,
+                "template parameter T must be signed integral type");
   return n / d - ((n ^ d) < 0 && n % d != 0);
 }
